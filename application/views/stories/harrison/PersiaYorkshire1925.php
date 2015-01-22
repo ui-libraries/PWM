1a48596e4af9bb413b5ea3dedfe3011fe8e74122
Mark NeuCollins <mneucollins@gmail.com>
2015-01-22 07:59:30 -0600

    persia and yorkshire

---
+++ application/views/stories/harrison/PersiaYorkshire1925.php
@@ -0,0 +1,31 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>Tornado of 1925</h2>
+    <h3>Persia and Yorkshire, Iowa</h3>
+     
+    <div class="leftcol500 nomargin">
+	    <p>On June 3, 1925, three tornadoes struck the towns of Persia and Yorkshire in the late afternoon between the hours of 4 and 5 p.m.</p>
+		<p>Both Persia and Yorkshire are located within a rich agricultural area in the southeastern part of Harrison County. Shortly after the construction of the Chicago Milwaukee and St. Paul Railroad in 1881, a blacksmith shop, grocery store and hotel were constructed through Persia. By 1883, the town was officially formed and recognized. The nearby town of Yorkshire came into existence around the same time. It was first platted in June of 1872 by the railroad company and a store developed shortly after (<i>Harrison County Bicentennial</i>). </p>
+		<p>Two of the June 3rd tornadoes hit Persia — impacting the southeastern and southwestern parts of town. Yorkshire was also struck by a tornado from the same storm and lost a depot, local store, church, schoolhouse and a number of residential homes.</p>
+		<p>In addition to the damage directly inflicted by the tornadoes themselves on Persia and Yorkshire, the storms produced “hail as big as baseballs” (<i>Persia Centennial</i>). Local creeks in the area also flooded severely after the storms passed through, adding to the damage.</p>
+	    <div class="vspacer"></div>
+	    <img class="alignleft" width="500" height="273" alt="Remains of machine shed" src="<?php echo image_url('harrison/PersiaYorkshire1925/PersiaAndYorkshire1.jpg');?>">
+	    <p class="caption">The remains of a machine shed on a local farm after the Yorkshire Tornado struck. [Credit: Then and Now: A Pictorial History of Harrison County]</p>
+    </div>
+    	
+	<div class="rightcol500 nomargin">
+	    <img class="alignleft" width="500" height="300" alt="Aftermath of Persia Tornado" src="<?php echo image_url('harrison/PersiaYorkshire1925/PersiaAndYorkshire2.jpg');?>">
+	    <p class="caption">People examining the ruins left by one of the Persia tornadoes. [Credit: Persia Centennial]</p>
+		<p>Clara Hamann vividly remembers one of the 1925 Persia tornadoes, writing about this event 58 years later in the <i>Persia Centennial</i>:</p> 
+		<blockquote><i>This is one experience in my life I will never forget as I was at the home of my brother-in-law Lester Hilborn. I heard him yelling, get the kids, blankets, axe, flashlight and get to the cellar as he was watching the storm coming. The sensation as he tried to hold all of us in her arms behind a bank in the cellar when the storm hit. Ripping off the cellar door and a basement window, moved the house from its foundation, and left just a shell. The terrible suction as if it was going to suck us right out of the cellar. Coming out of the cellar after the storm and seeing all the buildings gone, even the horses which had been tied in the barn. But how thankful we were to be alive and unhurt.</i></blockquote>
+		<p>In the days following the June 3rd storms, two thousand volunteers came from Cass, Harrison, Mitchell, Pottawattamie and Shelby counties to help the tornado refugees. Much of the volunteer work focused on assisting seventy-five farms that were damaged by the tornadoes. In addition, five thousand dollars along with much material and food aid was donated and distributed to help the storm refugees.</p> 
+	</div>
+
+	<div class="clearboth centered"><hr />[All of the above info comes from the <i>Persia Centennial</i>, unless otherwise cited]</div>
