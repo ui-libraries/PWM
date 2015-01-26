250c2bebf5d716032937c2174a8e0b433659abc1
Mark NeuCollins <mneucollins@gmail.com>
2015-01-25 18:55:54 -0600

    Logan 1999 tornaro story)

---
+++ application/views/stories/harrison/Logan1999.php
@@ -0,0 +1,70 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>Tornadoes of 1999</h2>
+
+    <h3>Logan Iowa</h3>
+
+    <!-- full width text -->
+    <p></p>
+    
+    <!-- left column text and image -->
+    <div class="leftcol500 nomargin">
+	    <img class="alignleft" width="100%"  alt="Path of 1999 Logan Tornado" src="<?php echo image_url('harrison/Logan1999/LoganMap.jpg');?>">
+	    <p class="caption">A storm chaser’s map of the two tornadoes around Logan. The tornado on the southeast of Logan caused the most damage and the tornado to the west damaged two homes. To read about this storm chaser’s experience, visit: <a href="http://extremeinstability.com/may_16th_storm_info.html">extremeinstability.com/may_16th_storm_info.html</a></p>
+</p>
+
+		<div class="vspacer"></div>
+    	
+    	<p>The storms around Logan destroyed six homes, a bridge on a gravel road and damaged many other homes in the area. Highway 30 and Highway 127 were both closed because of the debris strewn across them from the tornadoes.</p>
+
+		<p>Flooding followed the storms, as thunderstorms dumped more than three inches of rain across the county on Sunday, May 16th. Two days later, Iowa Governor Tom Vilsack declared Harrison County a disaster area because of the damage caused by the tornadoes. The storms caused an estimated $1.5 million in damage.</p>
+		
+		<p>Bobbie Sheranko, the mother of Juliet Pali who died in one of the tornadoes, turned 47 on the day she lost her daughter and was out of town in Texas when the tornado struck. Afterwards, while struggling with the loss of her daughter, Sheranko created a website to remember Juliet by and called it “Our Lovely Angel Juliet.” On the website, Sheranko recounts her search for others who had experienced the loss of a child. The Internet provided an outlet and a community of people to whom Sheranko expressed her feelings and thoughts.</p>
+		
+		<div class="vspacer"></div>
+	
+	    <img class="alignleft" width="100%"  alt="Volunteers helpng with clean-up" src="<?php echo image_url('harrison/Logan1999/Logan1999_2.jpg');?>">
+		
+    </div>
+ 
+ <!-- ------------------------------------------ -->       
+    <div class="rightcol500 nomargin">
+	    <p><span class="lead_in">Harrison County hadn’t seen a deadly tornado in over fifty years until May 16, 1999.</span> Many local high schoolers were celebrating their graduation that afternoon. The Logan-Magnolia High School concluded its graduation ceremony for the 41 graduating seniors around 3 pm. Just half an hour later, their superintendent announced a tornado warning over the school’s intercom.</p>
+
+		<p>Most of Iowa was under a tornado watch that day and the National Weather Service issued tornado warnings for more than a dozen counties. Two tornadoes touched down in the area surrounding the town of Logan, spinning off of a super cell weather event that swept through Western Iowa in the late afternoon that Sunday. While Crawford, Mills and Pottawattamie counties were all impacted by these storms, Harrison County was hit the hardest. </p>
+
+		<p>Two people died and fourteen were injured from the two tornadoes. Kathy Fugate (37 years old) and Juliet Pali (15 years old, of Bellevue) both lost their lives when trying to flee one of the tornadoes. Fugate and Pali, along with five other people, were celebrating the graduation of Kathy Fugate’s son when the storm hit. They decided Fugate’s trailer was unsafe and left to find a safer location. </p>
+
+		<p>They decided to seek shelter in the cellar of Beverly Harter’s farmhouse just down the road from Fugate’s home. The group of seven piled into a car and headed towards Harter’s home. When they were less than an eighth of a mile away, the tornado destroyed Harter’s house and headed directly towards their car.</p> 
+
+		<p>Seeing no other options, they jumped out of the car and sought shelter in the ditch. The tornado picked up and threw their car and a combine head on top of Beverly Harter, Kathy Fugate and Juliet Pali. The impact crushed and killed Fugate and Pali and Beverly Harter was paralyzed from the waist down. The five other people in the ditch were injured and three of them sustained serious injuries, including Fugate’s 15-year-old daughter, Lynette, and Lynette’s uncle, Kent Harter. Fugate’s mobile home remained in tact through the storm.</p>
+		
+		<p>A tornado that struck south of Logan caused the most damage, while another tornado moved west of Logan and damaged two homes. Both of these tornadoes were later classified as F-3 storms, with wind speeds between 158 and 205 mph.</p> 
+		
+		<div class="vspacer"></div>
+	
+
+	    <img class="alignleft" width="100%"  alt="National Weather Service Photo of Tornado Destruction" src="<?php echo image_url('harrison/Logan1999/Logan1999_1.jpg');?>">
+	    <p class="caption">National Weather Service aerial photograph from depicting the damage from one of the Logan tornadoes.</p>
+
+	    <div class="vspacer"></div>
+<p>In the days following the storms, volunteers came to help those affected in the area around Logan. It is recounted in the book, Then and Now: A Pictorial History of Harrison County, IA:</p>
+
+<p>Over the years, one thing that has not changed about Harrison County is the generosity of neighbors helping neighbors. Volunteers from all over the county, fire and rescue personnel, friends, family and even strangers came to help the victims with the clean-up.</p>
+
+
+	    <div class="vspacer"></div>
+	    <div class="vspacer"></div>
+		<p>
+			[All of the above information came from <a href="http://www.freewebs.com/ourlovelyangeljuliet/newsstoryaboutjuliet.htm">www.freewebs.com/ourlovelyangeljuliet/</a>, the <i>Estherville Daily News</i>, the <i>Oskaloosa Herald</i>, <a href = "http://www.city-data.com/">City-data.com</a>, and the <i>>Harrison County Happenings Newsletter</i>.]
+		</p>
+    </div>
+    <div class="clearboth"></div>
+    <div class="vspacer"></div>
