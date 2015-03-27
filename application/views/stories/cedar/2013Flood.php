6a99d7cf0db4b0c59b421e6035c8af976ded9079
Mark NeuCollins <mneucollins@gmail.com>
2015-03-27 10:57:16 -0500

    Added 2013Flood to Cedar County

---
+++ application/views/stories/cedar/2013Flood.php
@@ -0,0 +1,92 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>The June 24, 2013 Flash Flood</h2>
+    <h3>Lowden, Iowa</h3>
+    <p class="byline">Steve Gottschalk (Typed by Eunice Boedeker)</p>
+
+    <!-- full width text 
+    <p></p>
+    -->
+    
+    <!-- left column text and image -->
+    <div class="leftcol500 nomargin">
+	    <p><span class="lead_in">The Lowden area had just experienced its driest June</span> (from the 1st to the 20th) on record with just 0.22 inches of rain falling during the period. Who would have ever thought that we would have our worst flood in history, just four days later? This flash flood would surpass the famous one in 1944. Not only was the amount of rain greater in 2013, but also the rain fell in less than 6 hours compared to the flash flood of 1944, where rain fell over an 18-hour period.</p>
+	    <p>The weather forecast issued on the 23rd called for the possibility of heavy rains for the evening and morning hours of the 24th. At my midnight observation, I noticed the persistent lightning off to the southwest. I had seen this type of situation before where the storms form to our southwest and repeatedly move over our area to dump very heavy rains. </p>
+	    <p>The first wave of storms swept the area around 2 A.M. with high winds and torrential rains limiting visibility to less than a half block at times. Another similar storm followed an hour later. The storms continued to pound the area for the next five hours — dumping over 1 inch of rain per hour. There was continuous lightning, thunder and some hail. By 6 A.M., the Lowden area had received 4.10 inches and it was still raining. An hour later, the total was up to 5.30 inches. Just before 8 A.M., the recording rain gauge had recorded 6.62 inches had fallen in 5 ¾ hours. It soon went dead from floodwater and stopped recording. My other two rain gauges had floated away to be found later two blocks away. The Indee Creek (west part of town), East Creek (east part of town), and the Yankee Run Creek (south of town and U.S. 30) all overflowed because the Yankee Run filled up, causing the other two creeks to overflow into the western and eastern parts of town.</p>
+    	<img class="alignleft" width="100%"  alt="PK Midwest Farm Implements West of Lowden" src="<?php echo image_url('cedar/2013Flood/June2013Flood-1.jpg');?>"> 
+	    <div class="vspacer"></div>
+	    <p>As a Union Pacific crane working on the railroad west of Lowden began to sink in the softened ground and lean over the tracks, one mile west of town, P&K Equipment was surrounded by water. Water covered the area from P&K into west Lowden on Highway 30, 5th Street and Harding Avenue, where it surrounded many homes and West Side Trucking. The only way to get into town that morning was from the north. High water covered other roads leading into town for several hours, and the county bridge on Y14 south, suffering washout damage was closed. The trains didn’t run again until the crane could be made to stand upright. </p>
+    	<img class="alignleft" width="100%"  alt="Main Street Lowden" src="<?php echo image_url('cedar/2013Flood/June2013Flood-3-WestMainSt.jpg');?>"> 
+	    <div class="vspacer"></div>
+	    <div class="vspacer">&nbsp;</div>
+    	<img class="alignleft" width="100%"  alt="Reinbring Chicken House" src="<?php echo image_url('cedar/2013Flood/June2013Flood-6.jpg');?>"> 
+	    <div class="vspacer"></div>
+    	<!-- <img class="alignleft" width="100%"  alt="Brian Stephan in front of flooded Midland Supply" src="<?php echo image_url('linn/Midland/Brian.jpg');?>"> 
+	    <p class="caption"></p>
+    	-->
+
+    </div>    
+    <!-- right column text -->
+    <div class="rightcol500 nomargin">
+    	<img class="alignleft" width="100%"  alt="Lowden Downtown" src="<?php echo image_url('cedar/2013Flood/June2013Flood-7.jpg');?>"> 
+	    <div class="vspacer"></div>
+	    <p>About this time the floodwaters had surrounded my home. I had to be rescued by the Lowden Firefighters with their boat. The floodwaters were 5 ½ feet deep by the National Weather Service weather station on the south end of McKinley Avenue. I was not the only evacuee. At least five homes were evacuated and two businesses were damaged. Occupants of four of the flooded homes have relocated and the homes will be demolished.</p>
+    	<img class="alignleft" width="100%"  alt="Steve Gottschalk Home" src="<?php echo image_url('cedar/2013Flood/June2013Flood-4.jpg');?>"> 
+	    <div class="vspacer"></div>
+	    <p>Although the northeast part of the county received the worst damage, other sections suffered as well. In the central part, south of Tipton, volunteers helped rescue a cow-calf herd that had floated down Sugar Creek. High waters from this creek closed several roads. </p>
+    	<img class="alignleft" width="100%"  alt="Boettger - 103 Washington St" src="<?php echo image_url('cedar/2013Flood/June2013Flood-5.jpg');?>"> 
+	    <div class="vspacer"></div>
+		<p>[All Photos Source: Lowden Historical Society]</p>
+	    <div class="vspacer"></div>
+
+		<p></p>
+    </div>
+
+<!--
+second set of columns
+    <div class="clearboth"></div>
+    <div class="vspacer"></div>
+
+    <div class="leftcol500 nomargin">
+		<div class="vspacer"></div>
+	</div>
+
+    <div class="rightcol500 nomargin">
+        <div class="vspacer"></div>
+    </div>
+ -->
+<!--quickflip example    
+	<div class="quickFlip_1 centered" style=" width:500px; height:272px;" >
+        <div class="front">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+
+        <div class="back">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+    </div>	    
+
+<script type="text/javascript">
+$(document).ready(function(){
+        $(function() {
+            $('.quickFlip_1').quickFlip();
+        });
+    });
+
+</script>
+-->
