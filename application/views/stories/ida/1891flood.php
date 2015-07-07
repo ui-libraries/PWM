2dbbe39608e8c22085f14f93b660adf64f2e521f
Mark NeuCollins <mneucollins@gmail.com>
2015-07-06 21:39:28 -0500

    adds ida county and ida grove flood of 1891 story

---
+++ application/views/stories/ida/1891flood.php
@@ -0,0 +1,128 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>Flood of 1891</h2>
+    <h3>Ida Grove, Iowa</h3>
+    <p class="byline">by Nathaniel Otjen</p>
+
+    <!-- full width text 
+    <p></p>
+    -->
+    
+    <!-- left column text and image -->
+    <div class="leftcol500 nomargin">
+	    <p><span class="lead_in">Ida County residents usually remember the early 1890’s</span> as a time of severe drought, but one large rainstorm during the last week of June in 1891 halted the dry spell and caused the Maple River to overflow its banks and submerge much of Ida Grove. By the end of 1891 it was clear that the flood of that year was the worst the town of Ida Grove had ever experienced.</p>
+
+<p>The storm hit Ida County on June 23rd in the evening and deposited nine inches of rain. The rivers quickly swelled and overflowed, destroying homes, railroad tracks and bridges, telephone lines, and farmers’ livestock and crops. At least twenty-five homes completely flooded in Ida Grove, and the flood forced many residents to seek shelter on higher ground.</p>
+
+    </div>
+    
+    <div class="rightcol500 nomargin">
+	    <div class="vspacer"></div>
+	    <img class="alignleft" width="100%"  alt="" src="<?php echo image_url('ida/1891Flood/1.jpg');?>">
+	    <p class="caption">The floodwaters submerged much of Ida Grove.</p>
+	    <div class="vspacer"></div>
+    </div>
+
+
+    <div class="singlecol">
+<p>Two days later, the Cedar Rapids Gazette reported that rescue teams still had to wait for the floodwaters to recede before they could attempt to help stranded residents. The rising water submerged all of the railroads that led to Ida Grove. More rain fell on the morning of the 26th and the Gazette wrote, “All communication with the north is cut off.” </p>
+
+<p>On the following morning the Gazette’s front-page story read: </p>
+<blockquote ><i>A Trail of Ruin: The Destruction of Life and Property in Norhwestern [sic] Iowa Appalling—The Receding Waters Leave Hundreds of Head of Stock in the Broiling Sun and an Awful Stench Pervades the Air. Impossible to Obtain Full Particulars Yet—The Work of Removing the Debris Commenced in Earnest.</i></blockquote>
+
+<p>On the 26th the Gazette reported, “Everything is in the wildest confusion.” Four people drowned from the rising water in Ida Grove. Many of the town’s residents sought temporary shelter in the G.A.R. and Masonic hall for several days.</p>
+
+<p>By June 27, the Gazette called Ida County “A Sea of Water.”</p>
+
+<p>Coverage of the flood even made it into the June 27th edition of the Buffalo Courier in Buffalo, New York: “Maple River here is the highest ever known. The town is inundated and all the bridges are washed away.”</p>
+
+    </div>
+    
+    <div class="leftcol500 nomargin">
+	    <div class="vspacer"></div>
+	    <img class="alignleft" width="100%"  alt="" src="<?php echo image_url('ida/1891Flood/2.jpg');?>">
+	    <p class="caption">The rushing water carried this railroad bridge tract nearly ten miles downstream to Ida Grove from Galva.</p>
+<p>Two days after the nine-inch deluge, the Gazette reported, “At the present time it is impossible to estimate the damage to railroad property in that territory, but enough is known to show it will be very heavy.” In Ida County, the flood destroyed much of the Onawa railroad branch that ran fifty miles from Onawa to Ida Grove. The Gazette reported its railways were “entirely blockaded by the carrying away of bridges and washing away of road beds.”</p>
+
+<p>Despite the damage caused to the railways, workers dispatched immediately to repair the lines. The Gazette writes, “The Northwestern management has a repair crew and half a dozen pile drivers at each end of the break on its lines, ready to begin repairs, but the waters are still so high that work can not be proceeded with. It will take several days to put the line in condition for the resumption of traffic.”</p>
+
+<p>Railway workers quickly repaired the lines once the floodwaters receded in early July.</p>
+	
+	    <div class="vspacer"></div>
+	    <div class="vspacer"></div>
+<p><b>Sources:</b> The Cedar Rapids Gazette, History of Ida County, Buffalo Courier and <a href="http://www.idagroveia.com/Resources/pages/history.html">http://www.idagroveia.com/Resources/pages/history.html</a></p>
+    </div>
+
+    <div class="rightcol500 nomargin">
+
+
+<p>“I saw farmers walking in water up to their waists pushing rafts on which were loaded their families and household goods,” a man from Hornick, Iowa (forty miles west of Ida Grove) told the Gazette. The History of Ida County quotes Persis Babcock who recalled “the scene of her father and other men wading through the flood waters with children and belongings heaped upon their shoulders.” In Ida Grove, the Babcock family housed forty guests and fed them all breakfast the morning following the flood.</p> 
+
+<p>The flood also damaged many miles of railroad in Ida County. The railroad first came to Ida Grove only fourteen years prior. The first train passed through the town on October 26, 1877 and Ida Grove soon expanded from the industry brought by the new railway. By the early 1890’s, six railroad lines ran through the area surrounding Ida County: the Northwestern, Illinois Central, Milwaukee & St. Paul, Chicago, St. Paul and Minneapolis & Omaha railroads. The flood damaged all of these railways.</p> 
+	    <div class="vspacer"></div>
+	    <img class="alignleft" width="100%"  alt="" src="<?php echo image_url('ida/1891Flood/3.jpg');?>">
+	    <p class="caption">The flood stalled passenger trains attempting to pass through Ida Grove. Note the floodwaters in the background that rose high enough to cover the railroad tracks.</p>
+	    <div class="vspacer"></div>
+
+    </div>
+	    
+    
+    	<!-- <img class="alignleft" width="100%"  alt="Brian Stephan in front of flooded Midland Supply" src="<?php echo image_url('linn/Midland/Brian.jpg');?>"> 
+	    <p class="caption"></p>
+    	-->
+
+    </div>    
+    <!-- right column text -->
+
+<!--
+second set of columns
+    <div class="clearboth"></div>
+    <div class="vspacer"></div>
+
+    <div class="leftcol500 nomargin">
+		<div class="vspacer"></div>
+		<p></p>
+		<div class="vspacer"></div>
+	</div>
+
+    <div class="rightcol500 nomargin">
+        <p></p>
+        <div class="vspacer"></div>
+    </div>
+-->
+
+<!--quickflip example    
+	<div class="quickFlip_1 centered" style=" width:500px; height:272px;" >
+        <div class="front">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+
+        <div class="back">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+    </div>	    
+
+<script type="text/javascript">
+$(document).ready(function(){
+        $(function() {
+            $('.quickFlip_1').quickFlip();
+        });
+    });
+
+</script>
+-->
