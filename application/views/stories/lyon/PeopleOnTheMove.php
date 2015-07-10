0127ab77b9caeae2a87171185c444d08f2808bb9
Mark NeuCollins <mneucollins@gmail.com>
2015-07-09 22:31:17 -0500

    Lyon County + Story 1 added

---
+++ application/views/stories/lyon/PeopleOnTheMove.php
@@ -0,0 +1,106 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>People On the Move, Severe Weather, and Climate in Lyon County</h2>
+    <h3>Lyon County, Iowa</h3>
+    <p class="byline">Barbara Eckstein</p>
+
+    <!-- full width text 
+    <p></p>
+    -->
+    
+    <!-- left column text and image -->
+    <div class="leftcol500 nomargin">
+	     <p><span class="lead_in">Before Lyon County was formed independently (1872), even before its name was Buncombe County and before it was attached to Woodbury County (1851), the territory it occupies was an important gathering place for many different groups of people.</span>  At a site along Big Sioux River and Blood Run Creek, now remembered by the Blood Run National Historic Landmark, many native groups used the site as an intertribal and ceremonial center.  Starting as early as 900 C.E. and continuing until 1720, the Oneota, then the Ioway-Oto and the Omaha-Ponca built settlements there.  The Cheyenne and Arikara, then the Dakota peoples were all visitors.  Archeologists describe the site as “intensively occupied” from 1500 C.E. until the early 18th century when Dakota peoples probably drove out the Ioway, Oto, and the Omaha-Ponca.</p>
+ 
+<P>Blood Run was such an important crossroads for such a long time, that University of Wisconsin archaeologists and geographers began, in the 1960s, to search there for evidence of climate events from earlier centuries.  In the 1960s and 1980s they sought evidence to test hypotheses about climatic events in Late Prehistoric and Early Contact (with Europeans) periods in the Midwest.  “It was soil and charcoal samples in the undisturbed village deposits that were sought for climatic research investigations.”  Because the burial mound chosen was built over a site of occupation, they hoped to find undisturbed storage pits with evidence of climate conditions.  In the end, although they found other valuable artifacts, they did not find the undisturbed climate evidence that they needed—the railroad, gravel mining, helpful volunteers all had moved the kind of soil and charcoal specimens that might say something about climate. </P>
+
+	    <div class="vspacer"></div>
+ <img class="alignleft" width="100%"  alt="Blood Run Historic Landmark" src="<?php echo image_url('lyon/PeopleOnTheMOve/lyon-nhl-bloodrun.jpg');?>"> 
+	    <p class="caption">Blood Run Historic Landmark</p>
+	    <div class="vspacer"></div>
+ <img class="alignleft" width="100%"  alt="Blood Run Historic Landmark" src="<?php echo image_url('lyon/PeopleOnTheMOve/BloodRunUIA_MNH.jpg');?>"> 
+	    <p class="caption">Photo Courtesy The University of Iowa Museum of Natural History</p>
+	    <div class="vspacer"></div>
+<p>Nearly a century after the dedication of the courthouse, the 2010 census counted 11, 581 residents in Lyon County. Maybe one resident, Raymond E. Winter, speaks for others when, in his 21st-century memoir, Memories of a Country Boy, he laments the loss of his Lyon County family farm. “If you took a plane and flew over the Midwest countryside viewing mile after mile of farmland, still all producing crops, year after year, you will see something missing if you look closely.  Almost without fail you will find four groves of trees on every section of farmland or at least some indication of a grove having been there at some time.  But there are no buildings.  They have all been bull-dozed away to make room for more ‘progress.’ On every one of those places, a family lived, worked and played.  It was a wonderful idea.  What a shame that this great country couldn’t find a way to preserve the family farm, which was the backbone of the nation.  Bigger is not always better, but it seems we never learn.</p>
+
+<p>“Driving around the countryside several months ago, I found tears welling up in my eyes as I found that my boyhood neighborhood isn’t what I remember.  The farmhouse where I was born and spent the first fourteen years of my life is still there, along with most of the same out buildings.  But as I drove south a half mile, the Weerd Boomgaarden place is leveled and as I drove by the spot, I could almost hear the family playing that sweet mandolin and violin music for which they were noted.  A half mile south of there where our cousins, the Havilands lived, is nothing but crop land….And so it goes, mile after mile in Mid-America today.”</p>
+
+
+    	<!-- <img class="alignleft" width="100%"  alt="Brian Stephan in front of flooded Midland Supply" src="<?php echo image_url('linn/Midland/Brian.jpg');?>"> 
+	    <p class="caption"></p>
+    	-->
+
+    </div>    
+    <!-- right column text -->
+    <div class="rightcol500 nomargin">
+	    <div class="vspacer"></div>
+    	<img class="alignleft" width="100%" alt="Guillaume Delisle 1702 Map" src="<?php echo image_url('lyon/PeopleOnTheMove/Delisle.jpg');?>"> 
+	    <p class="caption">Guillaume Delisle 1702 Map</p>
+	    <p>An 1851 Treaty with the Sioux officially ceded the land that is now Lyon County although the United States later fought a war against the Dakotas in 1862 and the Dakota peoples continued to hunt, seasonally, in the Lyon County territory until 1869.  1851 was an especially challenging year for a new county of Iowa to attract settlers.  In the spring and summer of that year vast rains occurred across Iowa.  They were followed by heat.  In Early History of Lyon County, George Monlux reports that that summer’s severe weather across Iowa destroyed crops, produced famine, and was accompanied by the presence of cholera. </p> 
+
+<p>Although Monlux moved to Lyon County himself in 1879, he explains that earlier that decade settlers, trappers, and soldiers had their doubts about the fertility of the Lyon County land.  If flooding and its effects scared away Iowa settlers in 1851, it was drought and prairie fire that made people skeptical about Lyon County in 1870-71.  The Rock River was dry on sandbars for months; the Little Rock’s channel was dry for miles.  Early settlers and trappers speculated that the territory was too near what they understood to be the Great American Desert.  They noted the presence of buffalo grass and had learned that where there is buffalo grass, there is not enough rain for grain crops.  Some, Monlux writes, had been through Nebraska, the Dakotas, and Kansas and knew them to be buffalo grass country, “and they dreaded the drought and the scorching hot winds of that region.”</p> 
+
+<p>Nevertheless, an earlier Lyon County historian, S.C. Hyde, wrote in his Historical Sketch from 1873 that there was enough room in Lyon County for 1500 families, each with 160 acres (at $5/acre).  And settlers did come, including Monlux.  There were dry years, as predicted, but the grass in the sloughs, higher than a man’s head, and the blue joint and pea-vine hay in the creek and river bottoms proved a silver lining—at least until ditches were opened in the sloughs.  Monlux claims farmers always regretted opening these ditches that flooded and were impassable in the spring and that pulled water away from wells that then went dry.  Although a fire in slough grass with a high wind was a danger, especially for newcomers, controlled burning of a ton of slough grass, Monlux says, provides more heat than a ton of soft coal.  Feed and fuel.</p>  
+
+<p>When the county courthouse was dedicated in 1917, the population of the county was 15,362.  Whatever concerns early settlers had about Lyon County climate, many found home there.</p>
+<img class="alignleft" width="100%" alt="Lyon County Courthouse" src="<?php echo image_url('lyon/PeopleOnTheMove/LyonCountyCourthouse.jpg');?>"> 
+<p class="caption">Lyon County Courthouse</p>
+
+
+<p><b>Sources:</b>  
+Henning, Dale R. and Schermer, Shirley J.  <i>“Blood Run Archaeological Investigations.”</i>  Plains Anthropologist 49.192 (Nov 2004): 399-434; George Monlux, <i>Early History of Lyon County  (Rock Rapids, Iowa, 1908-09)</i>; S.C. Hyde, <i>Historical Sketch of Lyon County (1872/1873)</i>; “Fiftieth Anniversary of the Dedication of the Lyon County Court House 1917-1967”: pamphlet; Raymond E. Winter,<i> Memories of a County Boy (2002)</i>. Blood Run NHL Website: <a href="http://bloodrunnhl.com/time.html">http://bloodrunnhl.com/time.html</a></p> 
+		
+    </div>
+
+<!--
+second set of columns
+    <div class="clearboth"></div>
+    <div class="vspacer"></div>
+
+    <div class="leftcol500 nomargin">
+		<div class="vspacer"></div>
+		<p></p>
+		<div class="vspacer"></div>
+	</div>
+
+    <div class="rightcol500 nomargin">
+        <p></p>
+        <div class="vspacer"></div>
+    </div>
+-->
+
+<!--quickflip example    
+	<div class="quickFlip_1 centered" style=" width:500px; height:272px;" >
+        <div class="front">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+
+        <div class="back">
+            <img class="quickFlipCta" src="<?php echo image_url('xxxx.jpg') ?>">
+
+            <div class="caption">
+                click on image to flip
+            </div>
+        </div>
+    </div>	    
+
+<script type="text/javascript">
+$(document).ready(function(){
+        $(function() {
+            $('.quickFlip_1').quickFlip();
+        });
+    });
+
+</script>
+-->
