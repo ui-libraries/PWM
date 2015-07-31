39749817f86ca9c617979a69c99d00671342fe3b
Mark NeuCollins <mneucollins@gmail.com>
2015-07-30 22:02:10 -0500

    adds Lyon County 2014 flood

---
+++ application/views/stories/lyon/2014Flood.php
@@ -0,0 +1,48 @@
+    <div class="return_nav">
+        <?php
+        $this->load->helper('form');
+        echo form_open("home/view/$return");
+        echo form_submit('back', 'Return to Map');
+        echo form_close();
+        ?>
+    </div>
+
+    <h2>The Flood of June 2014</h2>
+    <h3>Rock Rapids, Lyon County, Iowa</h3>
+    <p class="byline">Barbara Eckstein</p>
+
+    <!-- full width text 
+	<div class="singlecol">
+	    <p></p>
+    </div>
+    -->
+    
+    <!-- left column text and image -->
+    <div class="leftcol500 nomargin">
+	    <p><span class="lead_in">On June 16, 2014, Siouxland News Channel KTIV reported that the Rock River at Rock Rapids would rise another four feet</span> and would then be three feet higher than it had been in 1993.  This was the prediction of the National Weather Service. The Sheriff’s Department of Lyon County warned that not all of the river could be successfully sandbagged, and so they were ordering a mandatory evacuation in parts of Rock Rapids and in all of Alvord.  They recommended against travel anywhere in Lyon County.  The Sheriff added, “This storm is unlike anything they’ve ever experienced here in Rock Rapids.” </p> 
+
+		<p>Apparently trouble began Saturday, June 14, when there was a breach on the Rock River at Blue Mound State Park in Luverne, Minnesota. That breach sent water flowing towards Rock Rapids.  Five inches of local rain that weekend compounded the problem.  By Sunday morning the river was at 13 feet.  Sandbagging began with many volunteers from Rock Rapids and other towns.  The plan was to prepare the city in twelve hours, but by Sunday evening, as things looked worse and worse, officials decided sandbagging needed to continue. </p> 
+
+		<p>That night—2a.m. Monday morning—the river crested at 24.02 feet.  With a new gage having been added to the river in 2001 and with it a different method of measurement, there was some confusion about whether 1993 or 2010 held the flood record.  The crest in 2010 had been 22.25 feet.  Either way, the 2014 flood was greater.  So great that the Rock Rapids water plant had to be shut down and a boil order was issued for the city that Monday.  But the rains and the rising waters were not done yet.</p>
+	    
+	    <div class="vspacer"></div>
+    	<img class="alignleft" width="100%"  alt="Flooded Houses" src="<?php echo image_url('lyon/2014Flood/LyonFlood-2014.jpg');?>"> 
+	    <div class="vspacer"></div>
+		<p>Lyon County residents had long expected less rain there than in other parts of Iowa.  The 1899 Iowa Geological Survey, volume 10, reported that the average rainfall in Lyon County was 8 inches less than in southern Iowa.  In fact, as recent as 2012 the county was experiencing a drought.  By September of that year parts of northwest Iowa had slipped into the most severe drought category:  exceptional drought.  And yet Harry Hillaker, State Climatologist, declared June 2014 the wettest month in Rock Rapids and in Sioux City in the history of state weather recordkeeping.  Whether 1993 or 2010 saw the second highest crest of the Rock River at Rock Rapids, there is no doubt that 2014 set a new record.</p>
+
+    </div>    
+    
+    
+    <!-- right column text -->
+    <div class="rightcol500 nomargin">
+	    <div class="vspacer"></div>
+    	<img class="alignleft" width="100%"  alt="Sandbaggers" src="<?php echo image_url('lyon/2014Flood/LyonSandbaggers2014.jpg');?>"> 
+    	<div class="vspacer"></div>
+
+    	<p>While some 30 residents huddled in the elementary school gym, assisted by the Red Cross, at 11:15 a.m. on Tuesday, June 17, the Rock River in Rock Rapids crested at 26.98 feet, twice the level of flood stage.  The June 25 Lyon County Reporter estimated that 60-80 structures in Rock Rapids were affected and were temporarily or permanently uninhabitable.  Rock Rapids mayor Jason Chase told reporters that the biggest battles would be Island Park, the inundated recreation area, and other properties that were a total loss.  He wanted to get displaced people back in homes quickly.  “I don’t think anyone thought it would be this bad,” he said. </p> 
+
+    	<img class="alignleft" width="100%"  alt="Flood-Destroyed House" src="<?php echo image_url('lyon/2014Flood/LyonFlood2014Two.jpg');?>"> 
+    </div>
+	<div class="singlecol">
+		<p>Sources:  <a href = "http://www.kwwl.com/story/19475328/drought-northwest-iowa-exceptional-hurricane-isaac-monitor-map">www.kwwl.com/story/19475328/drought-northwest-iowa-exceptional-hurricane-isaac-monitor-map</a> ; “2014 Flood, A Special Supplement to the Lyon County Reporter” </p>
+	</div>
